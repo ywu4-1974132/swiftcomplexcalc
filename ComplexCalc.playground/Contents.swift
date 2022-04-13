87c414f49af444fa8cc52a5c8dd7f503e01796f2
skywu717 <skywu@SkydeMacBook-Pro.local>
2022-04-12 19:29:22 -0700

    final

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -1,8 +1,82 @@
+import CoreGraphics
+import Foundation
 print("Welcome back to the UW Calculator")
 
 // Your job is to fill out Calculator so all the expressions
 // below both compile and return "true"
 class Calculator {
+    
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func add(_ args: [Int]) -> Int{
+        var total = 0
+        for i in args{
+            total += i
+        }
+        return total
+    }
+    
+    func multiply(_ args: [Int]) -> Int{
+        var total = 1
+        for i in args{
+            total *= i
+        }
+        return total
+    }
+    
+    func count(_ args: [Int]) -> Int{
+        return args.count
+    }
+    
+    func avg(_ args: [Int]) -> Int{
+        let total = add(args)
+        return total/args.count
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int{
+        return args.reduce(beg, op)
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int){
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int){
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func add(lhs: [String: Int] , rhs: [String: Int]) -> [String: Int]{
+        var res = [String: Int]()
+        res["x"] = lhs["x"]! + rhs["x"]!
+        res["y"] = lhs["y"]! + rhs["y"]!
+        return res
+    }
+    
+    func subtract(lhs: [String: Int] , rhs: [String: Int]) -> [String: Int]{
+        var res = [String: Int]()
+        res["x"] = lhs["x"]! - rhs["x"]!
+        res["y"] = lhs["y"]! - rhs["y"]!
+        return res
+    }
     
 }
 
@@ -17,10 +91,11 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
+
 
 calc.add([1, 2, 3, 4, 5]) == 15
 calc.multiply([1, 2, 3, 4, 5]) == 120
@@ -37,6 +112,7 @@ calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
 calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
     // this is (((((1 op 1) op 1) op 1) op 1) op 1)
 
+
 let p1 = (5, 5)
 let p2 = (12, -27)
 let p3 = (-4, 4)
@@ -50,3 +126,6 @@ let pd1 = ["x": 5, "y": 5]
 let pd2 = ["x": -4, "y": 4]
 calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9]
 calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1]
+
+
+
